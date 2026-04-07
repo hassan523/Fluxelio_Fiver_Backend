67f4547624ab1979dc3e103124b082638d875187
CreadivSol <business@creadivsol.com>
2026-04-07 16:23:35 +0500

    Update AllowedOrigins.js

--- utils/AllowedOrigins.js
+++ utils/AllowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["http://localhost:3000", "https://fluxelio.vercel.app","https://fluxelio.vercel.app/];
+export const allowedOrigins = ["http://localhost:3000", "https://fluxelio.vercel.app","https://fluxelio.vercel.app/"];
