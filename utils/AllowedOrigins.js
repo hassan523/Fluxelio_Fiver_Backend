d073e0413e5155a5a079ea2cb7fb9fc587a63f1d
hassan523 <hs542327@gmail.com>
2026-09-20 00:50:20 +0500

    push

--- utils/AllowedOrigins.js
+++ utils/AllowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["http://localhost:5000", "http://localhost:5000/", "http://localhost:3000", "https://fluxelio.vercel.app", "https://fluxelio.vercel.app/", "http://localhost:3000/"];
+export const allowedOrigins = ["http://localhost:5000", "http://localhost:5000/", "http://localhost:3000", "https://fluxelio-fiver.vercel.app", "https://fluxelio-fiver.vercel.app/", "http://localhost:3000/"];
